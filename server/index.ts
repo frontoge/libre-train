ec313558de3b83b21a5e3bd3d5ceeb2e0538b5bc
Frontoge <coltsman1011@gmail.com>
2025-07-27 21:23:26 -0400

    Add CORS support to backend

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,19 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import apiRouter from "./api/router";
 import { Request, Response } from "express";
+import cors from "cors";
 
-console.log("Hello via Bun!");
+console.log("Starting server...");
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+    // TODO change this to fill with frontend Deployment URL
+    origin: '*', // Adjust as necessary for CORS
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use("/api", apiRouter);
 
