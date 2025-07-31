3eed442d91ef12267054c312e453af038519b980
Frontoge <coltsman1011@gmail.com>
2025-07-31 18:26:34 -0400

    Add missing foreign key to ClientDailyLog

--- database/tables/ClientDailyLog.sql
+++ database/tables/ClientDailyLog.sql
@@ -16,5 +16,7 @@ CREATE TABLE
     `target_fat` int(11) DEFAULT NULL,
     `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
     `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    KEY `FK_CLIENTLOG_CLIENT` (`client_id`),
+    CONSTRAINT `FK_CLIENTLOG_CLIENT` FOREIGN KEY (`client_id`) REFERENCES `Client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
   ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
