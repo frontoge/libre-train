2198798dddf0797b73bb6edcc821846672764472
Frontoge <coltsman1011@gmail.com>
2026-02-07 13:53:18 -0500

    Update client schema to reference trainer and contacts

--- database/tables/Client.sql
+++ database/tables/Client.sql
@@ -1,16 +1,14 @@
-CREATE TABLE
-  `Client` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `first_name` varchar(100) NOT NULL,
-    `last_name` varchar(100) NOT NULL,
-    `age` int(10) unsigned DEFAULT NULL,
-    `height` int(10) unsigned DEFAULT NULL,
-    `email` varchar(100) DEFAULT NULL,
-    `phone` varchar(15) DEFAULT NULL,
-    `img` varchar(255) DEFAULT NULL,
-    `notes` varchar(500) DEFAULT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `email` (`email`)
-  ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
+CREATE TABLE `Client` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `height` int(10) unsigned DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `contactId` int(10) unsigned NOT NULL,
+  `trainerId` int(10) unsigned NOT NULL,
+  `isActive` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `Client_contact_FK` (`contactId`),
+  KEY `Client_trainer_FK` (`trainerId`),
+  CONSTRAINT `Client_contact_FK` FOREIGN KEY (`contactId`) REFERENCES `Contact` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Client_trainer_FK` FOREIGN KEY (`trainerId`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
--- database/tables/Users.sql
+++ database/tables/Users.sql
@@ -1,9 +1,11 @@
-CREATE TABLE
-  `User` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `username` varchar(32) NOT NULL,
-    `pass` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
-    PRIMARY KEY (`id`)
-  ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
+CREATE TABLE `User` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `contactId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `User_contact_FK` (`contactId`),
+  CONSTRAINT `User_contact_FK` FOREIGN KEY (`contactId`) REFERENCES `Contact` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
