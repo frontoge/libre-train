685b9d4d64db426491c84d14d250b295f007e11b
Frontoge <coltsman1011@gmail.com>
2026-02-07 12:09:12 -0500

    Add CRUD routes for Contact model

--- server/api/router.ts
+++ server/api/router.ts
@@ -3,14 +3,16 @@ import { handleHealthCheck } from "./handlers";
 import { Routes } from "../../shared/routes";
 import { handleGetClients, handleCreateClient, handleDailyUpdate, handleGetDashboard, handleGetDashboardSummary, handleDeleteClient } from "./handlers/client-handlers";
 import { handleAuthSignup, handleAuthLogin } from "./handlers/auth-handlers";
+import { handleCreateContact, handleDeleteContact, handleGetContactById, handleGetContacts, handleUpdateContact } from "./handlers/contact-handlers";
 
 const router = Router();
 
 router.get(Routes.Health, handleHealthCheck);
 
+// Client routes
 router.get(Routes.Clients, handleGetClients);
 router.post(Routes.Clients, handleCreateClient);
-router.post(`${Routes.Clients}/daily-update`, handleDailyUpdate); // Assuming this is the correct endpoint for daily updates
+router.post(`${Routes.Clients}/daily-update`, handleDailyUpdate);
 router.get(`${Routes.Clients}/dashboard`, handleGetDashboard);
 router.get(`${Routes.Clients}/dashboard/summary`, handleGetDashboardSummary);
 router.delete(`${Routes.Clients}/:id`, handleDeleteClient);
@@ -18,5 +20,12 @@ router.delete(`${Routes.Clients}/:id`, handleDeleteClient);
 //Auth routes
 router.post(Routes.AuthSingup, handleAuthSignup);
 router.post(Routes.AuthLogin, handleAuthLogin);
+
+// Contact routes
+router.get(Routes.Contacts, handleGetContacts);
+router.get(`${Routes.Contacts}/:id`, handleGetContactById);
+router.post(Routes.Contacts, handleCreateContact);
+router.put(`${Routes.Contacts}/:id`, handleUpdateContact);
+router.delete(`${Routes.Contacts}/:id`, handleDeleteContact);
 
 export default router;
--- shared/routes.ts
+++ shared/routes.ts
@@ -1,6 +1,7 @@
 export const Routes = {
     Health: "/health",
     Clients: "/clients",
+    Contacts: "/contact",
     AuthSingup: "/auth/signup",
     AuthLogin: "/auth/login",
 }
