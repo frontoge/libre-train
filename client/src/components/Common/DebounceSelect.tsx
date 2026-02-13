e61ed05a2bf8b0587004ac860aa0c6254d5880ed
Frontoge <coltsman1011@gmail.com>
2026-02-13 10:48:11 -0500

    Add client search component

---
+++ client/src/components/Common/DebounceSelect.tsx
@@ -0,0 +1,61 @@
+import { Select, Spin, type SelectProps } from "antd";
+import { useMemo, useRef, useState } from "react";
+import debounce from "lodash.debounce";
+
+
+export interface DebounceSelectProps<ValueType = any>
+  extends Omit<SelectProps<ValueType | ValueType[]>,  'children'> {
+  fetchOptions: (search: string) => Promise<ValueType[]>;
+  debounceTimeout?: number;
+}
+
+function DebounceSelect<
+  ValueType extends {
+    key?: string;
+    label: React.ReactNode;
+    value: string | number;
+  } = any,
+>({ fetchOptions, debounceTimeout = 300, ...props }: DebounceSelectProps<ValueType>) {
+  const [fetching, setFetching] = useState(false);
+  const [options, setOptions] = useState<ValueType[]>([]);
+  const fetchRef = useRef(0);
+
+  const debounceFetcher = useMemo(() => {
+    const loadOptions = (value: string) => {
+      fetchRef.current += 1;
+      const fetchId = fetchRef.current;
+      setOptions([]);
+      setFetching(true);
+
+      fetchOptions(value).then((newOptions) => {
+        if (fetchId !== fetchRef.current) {
+          // for fetch callback order
+          return;
+        }
+
+        setOptions(newOptions);
+        setFetching(false);
+      });
+    };
+
+    return debounce(loadOptions, debounceTimeout);
+  }, [fetchOptions, debounceTimeout]);
+
+  return (
+    <Select
+      labelInValue
+      filterOption={false}
+      showSearch
+      onSearch={debounceFetcher}
+      notFoundContent={fetching ? <Spin size="small" /> : 'No results found'}
+      {...props}
+      options={options}
+      optionRender={(option) => (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          {option.label}
+        </div>
+      )}
+    />
+  );
+}
+export default DebounceSelect;
