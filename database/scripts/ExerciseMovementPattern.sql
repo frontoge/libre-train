e186c71a53bdd0ed82e58792a20bfe184ee9949d
Frontoge <coltsman1011@gmail.com>
2026-02-15 13:51:51 -0500

    Add movement pattern table and scripts

---
+++ database/scripts/ExerciseMovementPattern.sql
@@ -0,0 +1,6 @@
+INSERT INTO ExerciseMovementPattern (id, pattern_name) VALUES
+    (1, 'Squat'),
+    (2, 'Hip Hinge'),
+    (3, 'Push'),
+    (4, 'Pull'),
+    (5, 'Vertical Press');
---
+++ database/tables/ExerciseMovementPattern.sql
@@ -0,0 +1,4 @@
+CREATE TABLE ExerciseMovementPattern (
+    id SERIAL PRIMARY KEY,
+    pattern_name VARCHAR(255) NOT NULL
+);
