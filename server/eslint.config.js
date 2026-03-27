2e471fe94735938e75b50c58339a5aba546b8949
Frontoge <coltsman1011@gmail.com>
2026-03-27 16:56:51 -0400

    Add eslint

---
+++ server/eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config([
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.ts'],
+		extends: [js.configs.recommended, tseslint.configs.recommended],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.node,
+		},
+		rules: {
+			'@typescript-eslint/no-empty-object-type': 'off',
+		},
+	},
+]);
